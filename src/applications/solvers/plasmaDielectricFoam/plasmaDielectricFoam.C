f11bfae8ed074b4f996c4d763cb35d672d98a4f2
Rention <r.pasolari@gmail.com>
2025-10-28 22:28:35 +0200

    Renamed plasmaFoam to plasmaDielectricFoam to be consistent with its functionality

---
+++ src/applications/solvers/plasmaDielectricFoam/plasmaDielectricFoam.C
@@ -0,0 +1,107 @@
+/*---------------------------------------------------------------------------*\
+License
+    This file is part of the foamPlasmaToolkit.
+
+    The foamPlasmaToolkit is not part of OpenFOAM but is developed using the
+    OpenFOAM framework and linked against OpenFOAM libraries.
+
+    Copyright (C) 2025 Rention Pasolari
+
+    This program is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    plasmaDielectricFoam
+
+Description
+    Transient solver for coupled fluid and dielectric domains developed for
+    plasma simulation purposes.
+
+Usage
+    \b plasmaDielectricFoam [OPTIONS]
+
+    Example:
+        plasmaDielectricFoam -case plasmaDielectricTest
+
+Author
+    Rention Pasolari
+    Contact: r.pasolari@gmail.com
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "regionProperties.H"
+#include "fvOptions.H"
+#include "coordinateSystem.H"
+#include "loopControl.H"
+
+#include "fvSolution.H"
+#include "solutionControl.H"
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Transient solver for coupled fluid and dielectric domains developed"
+        " for plasma simulation purposes."
+    );
+
+    #define NO_CONTROL
+    #define CREATE_MESH createMeshesPostProcess.H
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMeshes.H"
+    #include "createFields.H"
+
+    #include "createCoupledRegions.H"
+
+    #include "readNonCoupledElectricPotentialControls.H"
+
+    Info<< "\nStarting iteration loop\n" << endl;
+
+    while (runTime.run())
+    {
+        ++runTime;
+
+        Info << "Time = " << runTime.timeName() << nl << endl;
+        
+        // Solve the Poisson/Laplace Equation (electric potential)
+        if(coupled)
+        {
+            #include "solveElectricPotentialCoupled.H"
+        }
+        else
+        {
+            Info<< "Solving for ePotential in " 
+                << "non-coupled regions (segregate)" << endl;
+            for
+            (
+                int outer = 1;
+                outer <= ePotentialNonCoupledLoopMaxIter;
+                ++outer
+            )
+            {
+                #include "solveElectricPotentialNonCoupled.H"
+            }
+        }
+
+        runTime.write();
+        runTime.printExecutionTime(Info);
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
